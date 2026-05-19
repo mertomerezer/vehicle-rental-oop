7c3c4f32c2c6fb7612fad24c427df2b900323fe5
Mert Ömer Ezer <mertomer346@gmail.com>
2026-05-19 15:56:56 +0300

    OOP Kısmı bitti

--- src/RentalService.java
+++ src/RentalService.java
@@ -4,14 +4,47 @@ public class RentalService {
     Motorcycle motorcycle;
 
     public void rentCar(Customer c,int days){
+        if(car != null && !car.isRented() && c.getRentedVehicle() == null){
 
+            double price = car.calculateRent(days);
+            System.out.println("Price: " + price);
+
+            car.setRented(true);
+            c.setRentedVehicle(car);
+
+        } else {
+            System.out.println("Car cannot be rented");
+        }
     }
 
     public void rentTruck(Customer c,int days){
+        if(truck != null && !truck.isRented() && c.getRentedVehicle() == null){
+
+            double price = truck.calculateRent(days);
+            System.out.println("Price: " + price);
+
+            truck.setRented(true);
+            c.setRentedVehicle(truck);
+
+        } else {
+            System.out.println("Truck cannot be rented");
+        }
 
     }
 
     public void rentMotorcycle(Customer c,int days){
+        if(motorcycle != null && !motorcycle.isRented() && c.getRentedVehicle() == null){
+
+            double price = motorcycle.calculateRent(days);
+            System.out.println("Price: " + price);
+
+            motorcycle.setRented(true);
+            c.setRentedVehicle(motorcycle);
+
+        } else {
+            System.out.println("Motorcycle cannot be rented");
+        }
+
 
     }
 
